109947169d7af9674b2b600a13e2a6ef65144428
Green <green@gmail.com>
2017-09-05 18:47:06 +0300

    1

---
+++ Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("a21e518c-c132-4d1c-8e3a-7f539ac23051")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator/app.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+function makeBuffer() {
+    var buffer = "";
+
+    var buffObj = function (str) {
+        if (arguments.length === 0) return buffer;
+        else {
+            var v = new String(str);
+            buffer += v.toString();
+        }
+    };
+
+    buffObj.clear = function () {
+        buffer = "";
+    };
+    return buffObj;
+};
+
+var buffer = makeBuffer();
+
+var numbers = document.querySelectorAll(".number");
+for (var i = 0; i < numbers.length; i++) {
+    eventHolder(numbers[i]);
+};
+
+  
+function eventHolder(elem) {
+    elem.addEventListener("click",
+        function () {
+            console.log(elem.innerHTML);
+        });
+}
