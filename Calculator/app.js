3ff27948222f7a23693d7f1d54927aaa109c5c31
Green <green@gmail.com>
2017-09-06 12:12:08 +0300

    parser

--- Calculator/app.js
+++ Calculator/app.js
@@ -31,6 +31,11 @@ function makeBuffer() {
         return numStack.pop();
     }
 
+    buffObj.getLastNumber = function ()
+    {
+        return numStack[numStack.length - 1];
+    }
+
     buffObj.pushOperation = function (oper) {
         operStack.push(oper);
         console.log(operStack);
@@ -40,8 +45,67 @@ function makeBuffer() {
         return operStack.pop();
     }
 
+    buffObj.clearNumStack = function (){
+        numStack = [];
+    }
+
+    buffObj.clearOperStack = function () {
+        operStack = [];
+    }
+
+    buffObj.StackLength = function() {
+        var stackLength = new Object;
+        stackLength.numStack = numStack.length;
+        stackLength.operStack = operStack.length;
+        return stackLength;
+    }
+
     buffObj.calculate = function() {
+        if (numStack.length === operStack.length) var lastOper = operStack.pop();
+        var members = [];
+        var operations = [];
+        while (numStack.length) {
+            var tempNum = numStack.pop();
+            if (operStack.length) var tempOper = operStack.pop();
+            else tempOper = null;
+            if (tempOper === "*" || tempOper === "/") {
+                switch (tempOper) {
+                    case "*":
+                        members.push(tempNum * numStack.pop());
+                        break;
+                    case "/":
+                        members.push(numStack.pop() / tempNum);
+                        break;
+                }
+                if (operStack.length) operations.push(operStack.pop());
+            } else {
+                members.push(tempNum);
+                if (tempOper) operations.push(tempOper);
+            }
+            console.log(numStack);
+            console.log(operStack);
+            console.log(members);
+            console.log(operations);
+        }
+        var total = members.pop();
+        if (members.length && operations.length) {
+            while (members.length) {
+                var tempMember = members.pop();
+                var tempOperation = operations.pop();
+                switch (tempOperation) {
+                case "+":
+                    total += tempMember;
+                    break;
+                case "-":
+                    total -= tempMember;
+                    break;
+                }
+            }
+        }
         
+        if (lastOper) operStack = [lastOper];
+        numStack = [total];
+        return total;
     }
 
     return buffObj;
@@ -76,12 +140,14 @@ function operatorHolder(elem) {
             buffer.pushNumber(buffer.value());
             buffer.pushOperation(elem.innerHTML);
             buffer.clear();
-            buffer.hasPoint = false;
+            buffer.hasPoint = false;          
         });
 }
 
 document.querySelector("#AC").onclick = function() {
     buffer.clear();
+    buffer.clearNumStack();
+    buffer.clearOperStack();
     buffer.hasPoint = false;
     document.querySelector("#tableu").innerHTML = "0";
 }
@@ -97,8 +163,13 @@ document.querySelector("#point").onclick = function () {
 }
 
 document.querySelector("#equal").onclick = function () {
-    buffer.pushNumber(buffer.value());
-    buffer.clear();
-    buffer.hasPoint = false;
-    document.querySelector("#tableu").innerHTML = buffer.calculate();
+
+    if (buffer.value()) buffer.pushNumber(buffer.value());
+    else if (buffer.StackLength().numStack) buffer.pushNumber(buffer.getLastNumber());
+
+    if (buffer.StackLength().numStack > 1 && buffer.StackLength().operStack > 0) {
+        buffer.clear();
+        buffer.hasPoint = false;
+        document.querySelector("#tableu").innerHTML = buffer.calculate();
+    } 
 }
